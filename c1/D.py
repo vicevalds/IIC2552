ba1363a601af7de8cf8e57b312aab31b6ee9fe0a
vicevalds <vicentevaldesfigueroa@protonmail.com>
2025-03-16 22:23:02 -0300

    c1, D

--- c1/D.py
+++ c1/D.py
@@ -1,26 +1,16 @@
-from collections import deque
 
 d, c, r = ( int(n) for n in input().split() )
-con = deque(sorted([int(input()) for a in range(c)]))
-rev = deque(sorted([int(input()) for a in range(r)], reverse=True))
+con = [int(input()) for a in range(c)]
+rev = [int(input()) for a in range(r)]
 
 t = 0
-while d >= 0 and len(rev) > 0:
-    print(f"energia actual {d}")
-    print(f"tareas listas {t}")
-    print(f"con {con}")
-    print(f"rev {rev}")
-    if len(con) > 0 and d - con[0] >= 0:
-        v = con.popleft()
-        print(f"menos {v}")
-        d -= v
+d += sum(rev)
+t += len(rev)
+
+for a in con:
+    if a <= d:
+        d -= a
+        t += 1
     else:
-        rr = rev.popleft()
-        d += rr
-        print(f"mas {rr}")
-    t += 1
-#print(t)
-print(f"energia actual {d}")
-print(f"tareas listas {t}")
-print(f"con {con}")
-print(f"rev {rev}")
+        break
+print(t)
